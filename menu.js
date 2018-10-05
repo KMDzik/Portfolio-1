1274047d4e8d1bf29456b209ad6bd2b02561c6e0
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-05 21:10:27 +0200

    wip

--- menu.js
+++ menu.js
@@ -1,11 +1,11 @@
-$('<button class="close-button">\
-    <b class="visually-hidden">Close</b>\
-    <span aria-hidden="true">\
-      ×\
-    </span>\
-  </button>').appendTo(".main-nav");
+// $('<button class="close-button">\
+//     <b class="visually-hidden">Close</b>\
+//     <span aria-hidden="true">\
+//       ×\
+//     </span>\
+//   </button>').appendTo(".main-nav");
 
-$(".main-nav").on("click", function() {
-   $(this).toggleClass("open");
-  return false;
-});
+// $(".main-nav").on("click", function() {
+//    $(this).toggleClass("open");
+//   return false;
+// });
